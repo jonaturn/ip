6d815e7a5ac95fa7a201ed415a385f313442b5ac
= <jonathencheng86@gmail.com>
2025-01-22 23:35:28 +0800

    level 1 echo

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,17 +1,41 @@
+import java.util.Scanner;
+
 public class John {
     public static void main(String[] args) {
+        // init
+        Scanner sc = new Scanner(System.in);
+        String input;
         String logo = "         _       _           \n"
                 + "        | | ___ | |__  _ __  \n"
                 + "     _  | |/ _ \\| '_ \\| '_ \\\n"
                 + "    | |_| | (_) | | | | | | |\n"
                 + "     \\___/ \\___/|_| |_|_| |_|\n";
+
         System.out.println("Hello from\n" + logo);
 
+        greet();
+        input = sc.nextLine();
+        // System.out.println("\n");
+        while (!input.equals("bye")) {
+            System.out.println("--------------------------------------------------------------------\n"
+                    + input + "\n"
+                    + "--------------------------------------------------------------------\n");
+            input = sc.nextLine();
+        }
+        exit();
+    }
+
+    public static void greet() {
         System.out.println("--------------------------------------------------------------------\n"
                 + "Hello! I'm John\n"
                 + "What can I do for you?\n\n"
-                + "--------------------------------------------------------------------\n"
-                + "Bye. Hope to see you again soon!"
+                + "--------------------------------------------------------------------\n");
+
+    }
+
+    public static void exit() {
+        System.out.println("--------------------------------------------------------------------\n"
+                + "Bye. Hope to see you again soon!\n"
                 + "--------------------------------------------------------------------\n");
     }
 }
