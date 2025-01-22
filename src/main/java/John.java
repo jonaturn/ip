7baf44f0c89fbd066118effbd7ee73d031ab3e1d
= <jonathencheng86@gmail.com>
2025-01-22 23:19:13 +0800

    level-0

---
+++ src/main/java/John.java
@@ -0,0 +1,17 @@
+public class John {
+    public static void main(String[] args) {
+        String logo = "         _       _           \n"
+                + "        | | ___ | |__  _ __  \n"
+                + "     _  | |/ _ \\| '_ \\| '_ \\\n"
+                + "    | |_| | (_) | | | | | | |\n"
+                + "     \\___/ \\___/|_| |_|_| |_|\n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("--------------------------------------------------------------------\n"
+                + "Hello! I'm John\n"
+                + "What can I do for you?\n\n"
+                + "--------------------------------------------------------------------\n"
+                + "Bye. Hope to see you again soon!"
+                + "--------------------------------------------------------------------\n");
+    }
+}
