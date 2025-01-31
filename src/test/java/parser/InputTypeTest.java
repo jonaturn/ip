e6647b92854d3d17abafe29fbc530b35eb9d6a90
= <jonathencheng86@gmail.com>
2025-01-31 23:07:08 +0800

    add unit tests

---
+++ src/test/java/parser/InputTypeTest.java
@@ -0,0 +1,20 @@
+package parser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ *Tests the parser package
+ */
+
+
+class ParserTest {
+    /**
+     * Tests the inputType function
+     */
+    @Test
+    public void inputHandlingTest() {
+        assertEquals(,);
+    }
+}
