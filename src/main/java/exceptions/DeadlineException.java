c758684f8ba21250de48508a57f6833b4d93dddc
= <jonathencheng86@gmail.com>
2025-02-14 01:19:13 +0800

    Added Tagging:\n\n[tag] [index(int)] [tagDescription(String)]\n'tag' as keyword\nInteger for task index \nDescription of tag

--- src/main/java/exceptions/DeadlineException.java
+++ src/main/java/exceptions/DeadlineException.java
@@ -1,10 +1,16 @@
 package exceptions;
 
+/**
+ * Exception to handle deadline faults
+ */
 public class DeadlineException extends TaskException {
     public DeadlineException(String message) {
         super(message);
     }
 
+    /**
+     * New empty constructor with default message
+     */
     public DeadlineException() {
         super("\n    -----------------------------------------------------\r\n"
                 + "       The description of a deadline cannot be empty.\r\n"
--- src/main/java/exceptions/TodoException.java
+++ src/main/java/exceptions/TodoException.java
@@ -1,13 +1,21 @@
 package exceptions;
 
+/**
+ * Exception to handle todo faults
+ */
 public class TodoException extends TaskException {
-   public TodoException(String message) {
-       super(message);
-   }
+    public TodoException(String message) {
+        super(message);
+    }
 
-   public TodoException() {
-       super("\n    -----------------------------------------------------\r\n"
-               + "       The description of a todo cannot be empty.\r\n"
-               + "    -----------------------------------------------------");
-   }
+    /**
+     * Empty constructor used to construct default message
+     */
+    public TodoException() {
+        super(
+                "\n    -----------------------------------------------------\r\n"
+                        + "       The description of a todo cannot be empty.\r\n"
+                        + "    -----------------------------------------------------"
+        );
+    }
 }
