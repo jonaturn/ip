0b6285548a4960abaa5394da50d33933c0cfd93b
= <jonathencheng86@gmail.com>
2025-02-20 23:17:48 +0800

    Add resize window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setMinHeight(600);
+            stage.setMinWidth(417);
             fxmlLoader.<MainWindow>getController().setJohn(john);
             stage.show();
         } catch (IOException e) {
